690c750db3bb2a485ee5aa0f11f5fc4c9ed5b45a
cmbauer2 <cmbauer2@yahoo.com>
2025-05-02 22:02:32 -0400

    task 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,50 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    let total= 0;
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = parseFloat(item.cost.substring(1)); // Convert cost from string to number
+      total += quantity * cost;
+    });
+    return total.toFixed(2);
  
   };
 
-  const handleContinueShopping = (e) => {
+  const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+      };
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
-  };
+  if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
-  const handleRemove = (item) => {
-  };
+    const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
+      };
+  
 
   // Calculate total cost based on quantity for an item
-  const calculateTotalCost = (item) => {
+  
+const calculateTotalCost = (item) => {
+    const quantity = item.quantity;
+    const unitPrice = parseFloat(item.cost.substring(1)); // Convert cost from string to number
+    const totalCost = quantity * unitPrice;
+    return totalCost.toFixed(2); // Return total cost as a string with two decimal places
   };
+  
 
   return (
     <div className="cart-container">
@@ -61,7 +83,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
     </div>
   );
-};
+
 
 export default CartItem;
 
